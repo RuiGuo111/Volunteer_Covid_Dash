cc219ca7cb7438d058654407ca8b2febde433e23
RuiGuo111 <rguo@falcon.bentley.edu>
2021-05-17 17:45:48 -0400

    Update app.py

--- app.py
+++ app.py
@@ -117,7 +117,7 @@ def update_piechart(state,status,gender,phase):
                            (df1['Gender'] == gender) & (df1['Phases'] == phase)]
     fig = px.pie(df2,
                  values = df2.City.value_counts(),
-                 names = df2.City.unique(),height = 100)
+                 names = df2.City.unique(),height = 500)
     fig.update_traces(textposition='inside', textinfo='percent+label')
     fig.update_layout(title = str(len(df2)) + " clinical trials are found",                     
                       font=dict(
